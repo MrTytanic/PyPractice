d5a8b27a9b48285f7c0a5d1484e833e4aa885605
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-04 18:37:36 -0400

    Add files via upload

---
+++ Lessons/Dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionary = a collection of {key:value} pairs
+
+capitals = {"USA": "Washington D.C.",
+                    "India": "New Delhi",
+                    "China": "Beijing",
+                    "Russia": "Moscow"}
+
+items = capitals.items()
+for key, value in capitals.items():
+    print(f"{key}: {value}")
