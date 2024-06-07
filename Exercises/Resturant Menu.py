c1e48c041c392de357d4fa5b45adcf1429acce85
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-06 23:21:02 -0400

    Add files via upload

---
+++ Exercises/Resturant Menu.py
@@ -0,0 +1,23 @@
+# Resturant Menu
+
+entree = {"steak": 43.00,
+        "salad": 15.00,
+        "shrimp": 35.000,
+        "lobster": 50.00,
+        "pasta": 30.00}
+
+beverage = {"water": 5.00,
+            "soda": 6.00,
+            "wine": 12.00}
+
+menu_prompt = input("Waiter: Would you like to see our menu?: ")
+if menu_prompt == "yes":
+    print("Okay, here is a list of our items and their prices")
+    print(entree)
+elif menu_prompt == "no":
+    print("Sir you need the menu to order but okay...")
+    input("What would you like to order?: ")
+else:
+    print("Sir I cannot understand what you are saying...")
+    input("Do you want our menu or not?: ")
+
