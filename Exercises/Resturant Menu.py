8e153689f1bdfe75f50b1967503857d94547d255
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-06 23:35:25 -0400

    Update Resturant Menu.py

--- Exercises/Resturant Menu.py
+++ Exercises/Resturant Menu.py
@@ -1,4 +1,4 @@
-# Resturant Menu
+# Resturant Menu - A humours interactive "game" that utilizes dictionaries
 
 entree = {"steak": 43.00,
         "salad": 15.00,
@@ -10,6 +10,10 @@ beverage = {"water": 5.00,
             "soda": 6.00,
             "wine": 12.00}
 
+bill = []
+total = 0
+
+# prompt the user to use the menu
 menu_prompt = input("Waiter: Would you like to see our menu?: ")
 if menu_prompt == "yes":
     print("Okay, here is a list of our items and their prices")
@@ -19,5 +23,3 @@ elif menu_prompt == "no":
     input("What would you like to order?: ")
 else:
     print("Sir I cannot understand what you are saying...")
-    input("Do you want our menu or not?: ")
-
