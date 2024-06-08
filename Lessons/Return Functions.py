13b614db29783539f40637be30f504edd764ae97
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-08 02:17:49 -0400

    Add files via upload

---
+++ Lessons/Return Functions.py
@@ -0,0 +1,22 @@
+# return functions 6/8
+
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+def divide(x, y):
+    z = x / y
+    return z
+
+print(add(1, 2))
+print(subtract(3, 4))
+print(multiply(5, 6))
+print(divide(7, 8))
