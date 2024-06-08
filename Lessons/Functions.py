1e78cf4ad92f59d7eab43be21336cee3c1fd180c
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-08 02:17:00 -0400

    Add files via upload

---
+++ Lessons/Functions.py
@@ -0,0 +1,11 @@
+
+def coding_message(language, difficulty):
+    print("Today I am learning about functions!")
+    print("These help to make repeating lines of code less messy.")
+    print(f"{language} is a {difficulty} language, I like coding so far.")
+    print("Okay I think I said enough.")
+    print()
+
+coding_message("Python", "simple")
+coding_message("Java", "great")
+coding_message("C++", "repulsive")
