f135209b4b39eb410e5521761385948f76698f12
MrTytanic <156623056+MrTytanic@users.noreply.github.com>
2024-06-17 21:36:24 -0400

    Add files via upload

---
+++ Lessons/module.py
@@ -0,0 +1,47 @@
+# scope resolution
+
+# local
+
+def func1():
+    x = 1 #local
+    print(x)
+
+def func2():
+    x = 2 #local
+    print(x)
+
+func1()
+func2()
+
+# enclosed
+
+def func1():
+    x = 1 #enclosed
+
+    def func2():
+        print(x)
+    func2()
+
+func1()
+
+# global
+
+def func1():
+    print(x)
+
+def func2():
+    print(x)
+
+x = 3 #global
+
+func1()
+func2()
+
+# built in
+
+from math import e
+
+def func1():
+    print(e)
+
+func1()
